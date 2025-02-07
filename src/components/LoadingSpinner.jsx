4f4363d7d5506046e367eacf5149fc12a19760f5
Paweesuda-Thippayanasa <paweesuda.thippayanasa@gmail.com>
2025-02-07 18:01:15 +0700

    update spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
+import './css/spiner.css'
 
 const LoadingSpinner = () => {
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-100">
-      <div className="relative z-10">
-        {/* Outer spinner */}
-        <div className="w-16 h-16 border-4 border-pink-200 border-t-orange-500 rounded-full animate-spin" 
-             style={{ animationDuration: '1s' }}
-        />
-        {/* Inner spinner */}
-        <div className="absolute top-2 left-2 w-12 h-12 border-4 border-blue-900 border-t-transparent rounded-full"
-             style={{ animation: 'spin 2s linear infinite reverse' }}
-        />
+    <div className="loader-container">
+      <div className="loader-wrapper">
+        <div className="loader">
+          <div className="loader-inner"></div>
+        </div>
       </div>
-      <p className="mt-4 text-lg font-medium text-blue-900 animate-pulse">
-        Loading amazing projects...
-      </p>
+      <p className="loading-text">Loading amazing projects...</p>
     </div>
   );
 };
